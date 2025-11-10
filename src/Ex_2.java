a7b14eadcca08dce25814c6fbe38a66da0e84ede
José Froes <josepaulo.froes@gmail.com>
2025-11-10 13:32:23 -0300

    Exercício 2

---
+++ src/Ex_2.java
@@ -0,0 +1,11 @@
+public class Ex_2 {
+
+// Calcule o resto da divisão entre dois números.
+
+    public static void main(String[] args) {
+        int n1 = 7;
+        int n2 = 19;
+
+        System.out.println(n1 + " % "  + n2 + " = " + (n1 % n2));
+    }
+}
