caa17c0f4bccc876901457576a0a6d71a78d06f6
José Froes <josepaulo.froes@gmail.com>
2025-11-10 14:59:47 -0300

    Exercício 8

---
+++ src/Ex_8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex_8 {
+
+// Use operadores bitwise para calcular a & b e a | b.
+
+    public static void main(String[] args) {
+
+        int a = 6;
+        int b = 3;
+
+        System.out.println("a & b = " + (a & b));
+        System.out.println("a | b = " + (a | b));
+    }
+}
