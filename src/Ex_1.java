bd421afd0e0e4d0f6e232eb0bbcad19dd437fb46
José Froes <josepaulo.froes@gmail.com>
2025-11-10 13:26:04 -0300

    atualizando

---
+++ src/Ex_1.java
@@ -0,0 +1,8 @@
+public class Ex_1 {
+
+//    Declare duas variáveis inteiras e exiba a soma, subtração, multiplicação e divisão.
+
+    public static void main(String[] args) {
+        int n1 = ;
+    }
+}
