e9e80e33ffc9b6ce1f503f538eca8c44b7040fd1
José Froes <josepaulo.froes@gmail.com>
2025-11-10 15:01:00 -0300

    Exercício 9

---
+++ src/Ex_9.java
@@ -0,0 +1,12 @@
+public class Ex_9 {
+
+// Mostre o resultado de ++ e -- aplicados em variáveis.
+
+    public static void main(String[] args) {
+
+        int a = 6;
+
+        System.out.println("++a = " + (++a));
+        System.out.println("--a = " + (--a));
+    }
+}
