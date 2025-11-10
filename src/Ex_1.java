5368152ea2cc30659e9806d3658a212c4509e0a0
José Froes <josepaulo.froes@gmail.com>
2025-11-10 13:31:11 -0300

    Exercício 1

--- src/Ex_1.java
+++ src/Ex_1.java
@@ -3,6 +3,12 @@ public class Ex_1 {
 //    Declare duas variáveis inteiras e exiba a soma, subtração, multiplicação e divisão.
 
     public static void main(String[] args) {
-        int n1 = ;
+        int n1 = 7;
+        int n2 = 19;
+
+        System.out.println(n1 + " + "  + n2 + " = " + (n1 + n2));
+        System.out.println(n1 + " - "  + n2 + " = " +  (n1 -  n2));
+        System.out.println(n1 + " * "  + n2 + " = " + (n1 * n2));
+        System.out.println(n1 + " / "  + n2 + " = " + (n1 / n2));
     }
 }
